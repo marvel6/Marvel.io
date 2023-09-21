e6faa106258741a89b9bf222724723b841ffd833
marvel6 <Marvelloussolomon49@gmail.com>
2023-09-21 18:07:06 +0100

    marvel.io configures

---
+++ public/uiStuff.js
@@ -0,0 +1,8 @@
+let Wheight = window.innerHeight
+let wWidth = window.innerWidth
+
+const canvas = document.querySelector('#the-canvas')
+const context = canvas.getContext('2d')
+
+canvas.height = Wheight
+canvas.width = wWidth
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const socketio = require('socket.io')
+const expressServer = app.listen(8000)
+const io = socketio(expressServer)
+
+
+app.use(express.static('./public'))
+
+
+
+
+module.exports = {
+    io,
+    app
+}
