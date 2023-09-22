2d56813228c405ef87285691458608e2e01cacf5
marvel6 <Marvelloussolomon49@gmail.com>
2023-09-22 01:03:36 +0100

    drew Orbjects dots 🍇

--- public/canvasStuff.js
+++ public/canvasStuff.js
@@ -14,10 +14,10 @@ player.locY = Math.floor(500 * Math.random() + 10)
 
 const draw = () => {
 
-    context.clearRect(0, 0, canvas.width, canvas.height)
-
 
     context.setTransform(1, 0, 0, 1, 0, 0)
+
+    context.clearRect(0, 0, canvas.width, canvas.height)
 
 
     const camX = -player.locX + canvas.width / 2
@@ -28,11 +28,19 @@ const draw = () => {
     context.beginPath()
     context.fillStyle = 'rgb(255,0,0)'
     context.arc(player.locX, player.locY, 10, 0, Math.PI * 2)
-    context.arc(200, 200, 10, 0, Math.PI * 2)
     context.fill()
-    context.lineWidth = 2
+    context.lineWidth = 3
     context.strokeStyle = 'rgb(0,255,0)'
     context.stroke()
+
+    orbs.forEach(el => {
+
+        context.beginPath()
+        context.fillStyle = el.color
+        context.arc(el.locX, el.locY, el.radius, 0, Math.PI * 2)
+
+        context.fill()
+    })
 
     requestAnimationFrame(draw)
 
